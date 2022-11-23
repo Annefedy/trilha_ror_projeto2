04100578457a803cb7d195998fe927ef0cb3ad81
alexandroabade <alexandroabade@gmail.com>
2022-11-23 18:29:26 -0300

    added first test case

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context 'before publication' do
+      it 'cannot have comments' do
+    pending "add some examples to (or delete) #{__FILE__}"
+      end
+   end
 end
